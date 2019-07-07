307c02a673d0cddb2c456e9d947089ea8d998e35
asamuzaK (Kazz) <kazz@asamuzak.jp>
2019-07-07 11:30:40 +0900

    Add MailExtentions handler

--- test/update.test.js
+++ test/update.test.js
@@ -2,7 +2,8 @@
 "use strict";
 const {
   createUnifiedSchema, fetchText, getAllSchemaData, getChannelUrl, getFileList,
-  getSchemaData, saveSchemaFile, updateSchemas,
+  getListedSchemaData, getMailExtSchemaData, getSchemaData, saveSchemaFile,
+  updateSchemas,
 } = require("../modules/update");
 const {assert} = require("chai");
 const {describe, it} = require("mocha");
@@ -81,36 +82,19 @@ describe("get channel url", () => {
   });
 
   it("should get result", () => {
+    const res = getChannelUrl("mail");
+    assert.strictEqual(
+      res, "https://hg.mozilla.org/comm-central/raw-file/tip/",
+      "result"
+    );
+  });
+
+  it("should get result", () => {
     const res = getChannelUrl("release");
     assert.strictEqual(
       res, "https://hg.mozilla.org/releases/mozilla-release/raw-file/tip/",
       "result"
     );
-  });
-});
-
-describe("get schema file list from jar manifest", () => {
-  it("should throw", async () => {
-    await getFileList().catch(e => {
-      assert.instanceOf(e, TypeError, "error");
-      assert.strictEqual(e.message, "Expected String but got Undefined.");
-    });
-  });
-
-  it("should get array", async () => {
-    const stubFetch = sinon.stub(fetch, "Promise").resolves({
-      ok: true,
-      status: 200,
-      text: () => "# comment\n\ntoolkit.jar:\n% content extensions %content/extensions/\n    content/extensions/schemas/alarms.json\n    content/extensions/schemas/browser_settings.json\n#ifndef ANDROID\n    content/extensions/schemas/geckoProfiler.json\n#endif\n    content/extensions/schemas/i18n.json\n",
-    });
-    const res = await getFileList("https://example.com");
-    stubFetch.restore();
-    assert.deepEqual(res, [
-      "alarms.json",
-      "browser_settings.json",
-      "geckoProfiler.json",
-      "i18n.json",
-    ], "result");
   });
 });
 
@@ -143,6 +127,31 @@ describe("get schema data", () => {
         foo: ["bar"],
       },
     }, "result");
+  });
+});
+
+describe("get schema file list from jar manifest", () => {
+  it("should throw", async () => {
+    await getFileList().catch(e => {
+      assert.instanceOf(e, TypeError, "error");
+      assert.strictEqual(e.message, "Expected String but got Undefined.");
+    });
+  });
+
+  it("should get array", async () => {
+    const stubFetch = sinon.stub(fetch, "Promise").resolves({
+      ok: true,
+      status: 200,
+      text: () => "# comment\n\ntoolkit.jar:\n% content extensions %content/extensions/\n    content/extensions/schemas/alarms.json\n    content/extensions/schemas/browser_settings.json\n#ifndef ANDROID\n    content/extensions/schemas/geckoProfiler.json\n#endif\n    content/extensions/schemas/i18n.json\n",
+    });
+    const res = await getFileList("https://example.com");
+    stubFetch.restore();
+    assert.deepEqual(res, [
+      "alarms.json",
+      "browser_settings.json",
+      "geckoProfiler.json",
+      "i18n.json",
+    ], "result");
   });
 });
 
@@ -190,8 +199,122 @@ describe("get all schema data", () => {
   });
 });
 
+describe("get specified schema data", () => {
+  it("should throw", async () => {
+    await getListedSchemaData().catch(e => {
+      assert.instanceOf(e, TypeError, "error");
+      assert.strictEqual(e.message, "Expected String but got Undefined.");
+    });
+  });
+
+  it("should throw", async () => {
+    await getListedSchemaData("foo").catch(e => {
+      assert.instanceOf(e, TypeError, "error");
+      assert.strictEqual(e.message, "Expected Array but got Undefined.");
+    });
+  });
+
+  it("should get array", async () => {
+    const stubFetch = sinon.stub(fetch, "Promise");
+    stubFetch.onCall(0).resolves({
+      ok: true,
+      status: 200,
+      text: () => "{\"foo\": \"foobar\"}",
+    });
+    stubFetch.onCall(1).resolves({
+      ok: true,
+      status: 200,
+      text: () => "{\"baz\": \"qux\"}",
+    });
+    const res = await getListedSchemaData("https://example.com/",
+                                          ["foo.json", "bar.json"]);
+    stubFetch.restore();
+    assert.deepEqual(res, [
+      {
+        file: "foo.json",
+        schema: {
+          foo: "foobar",
+        },
+      },
+      {
+        file: "bar.json",
+        schema: {
+          baz: "qux",
+        },
+      },
+    ], "result");
+  });
+});
+
+describe("get MailExtensions schema data", () => {
+  it("should throw", async () => {
+    await getMailExtSchemaData().catch(e => {
+      assert.instanceOf(e, TypeError, "error");
+      assert.strictEqual(e.message, "Expected String but got Undefined.");
+    });
+  });
+
+  it("should get array", async () => {
+    const stubFetch = sinon.stub(fetch, "Promise");
+    stubFetch.onCall(0).resolves({
+      ok: true,
+      status: 200,
+      text: () => "# comment\n\nmessenger.jar:\n% content/messenger/ext-mail.json\n    content/messenger/schemas/accounts.json\n    content/messenger/schemas/browserAction.json\n    content/messenger/schemas/commands.json\n    content/messenger/schemas/pkcs11.json\n",
+    });
+    stubFetch.onCall(1).resolves({
+      ok: true,
+      status: 200,
+      text: () => "{\"foo\": \"foobar\"}",
+    });
+    stubFetch.onCall(2).resolves({
+      ok: true,
+      status: 200,
+      text: () => "{\"bar\": \"baz\"}",
+    });
+    const res = await getMailExtSchemaData("https://example.com/");
+    stubFetch.restore();
+    assert.deepEqual(res, [
+      {
+        file: "accounts.json",
+        schema: {
+          foo: "foobar",
+        },
+      },
+      {
+        file: "browserAction.json",
+        schema: {
+          bar: "baz",
+        },
+      },
+    ], "result");
+  });
+});
+
 describe("create unified schema", () => {
   it("should get object", async () => {
+    const stubAll = sinon.stub(Promise, "all").resolves([]);
+    const res = await createUnifiedSchema();
+    stubAll.restore();
+    assert.deepEqual(res, {}, "result");
+  });
+
+  it("should get object", async () => {
+    const stubAll = sinon.stub(Promise, "all").resolves([]);
+    const res = await createUnifiedSchema("mail");
+    stubAll.restore();
+    assert.deepEqual(res, {}, "result");
+  });
+});
+
+describe("save schema file", () => {
+  it("should throw", async () => {
+    await saveSchemaFile().catch(e => {
+      assert.instanceOf(e, TypeError, "error");
+      assert.strictEqual(e.message, "Expected String but got Undefined.");
+    });
+  });
+
+  it("should create file", async () => {
     const stubAll = sinon.stub(Promise, "all").resolves([
       [
         {
@@ -210,25 +333,57 @@ describe("create unified schema", () => {
         },
       ],
     ]);
-    const res = await createUnifiedSchema();
+    const stubWrite = sinon.stub(fs.promises, "writeFile");
+    const stubInfo = sinon.stub(console, "info");
+    const i = stubWrite.callCount;
+    const j = stubInfo.callCount;
+    const filePath =
+      path.join(process.cwd(), "schemas", "mail", "mailext.json");
+    const res = await saveSchemaFile("mail");
+    const {callCount: writeCallCount} = stubWrite;
+    const {callCount: infoCallCount} = stubInfo;
+    stubInfo.restore();
     stubAll.restore();
-    assert.deepEqual(res, {
-      foo: {
-        foo: "bar",
-      },
-      baz: {
-        baz: "qux",
-      },
-    }, "result");
+    stubWrite.restore();
+    assert.strictEqual(writeCallCount, i + 1, "write");
+    assert.strictEqual(infoCallCount, j, "info");
+    assert.strictEqual(res, filePath, "result");
   });
-});
 
-describe("save schema file", () => {
-  it("should throw", async () => {
-    await saveSchemaFile().catch(e => {
-      assert.instanceOf(e, TypeError, "error");
-      assert.strictEqual(e.message, "Expected String but got Undefined.");
-    });
+  it("should create file", async () => {
+    const stubAll = sinon.stub(Promise, "all").resolves([
+      [
+        {
+          file: "foo",
+          schema: {
+            foo: "bar",
+          },
+        },
+      ],
+      [
+        {
+          file: "baz",
+          schema: {
+            baz: "qux",
+          },
+        },
+      ],
+    ]);
+    const stubWrite = sinon.stub(fs.promises, "writeFile");
+    const stubInfo = sinon.stub(console, "info");
+    const i = stubWrite.callCount;
+    const j = stubInfo.callCount;
+    const filePath =
+      path.join(process.cwd(), "schemas", "mail", "mailext.json");
+    const res = await saveSchemaFile("mail", true);
+    const {callCount: writeCallCount} = stubWrite;
+    const {callCount: infoCallCount} = stubInfo;
+    stubInfo.restore();
+    stubWrite.restore();
+    stubAll.restore();
+    assert.strictEqual(writeCallCount, i + 1, "write");
+    assert.strictEqual(infoCallCount, j + 1, "info");
+    assert.strictEqual(res, filePath, "result");
   });
 
   it("should create file", async () => {
@@ -309,6 +464,34 @@ describe("update schemas files", () => {
     const stubAll = sinon.stub(Promise, "all").callsFake(async arr => {
       let val;
       switch (arr.length) {
+        case 3:
+          val = [
+            [
+              {
+                file: "foo",
+                schema: {
+                  foo: "baz",
+                },
+              },
+            ],
+            [
+              {
+                file: "baz",
+                schema: {
+                  baz: "qux",
+                },
+              },
+            ],
+            [
+              {
+                file: "foobar",
+                schema: {
+                  foobar: "quux",
+                },
+              },
+            ],
+          ];
+          break;
         case 2:
           val = [
             [
@@ -347,18 +530,49 @@ describe("update schemas files", () => {
       path.join(process.cwd(), "schemas", "central", "webext.json");
     const releasePath =
       path.join(process.cwd(), "schemas", "release", "webext.json");
+    const mailPath =
+      path.join(process.cwd(), "schemas", "mail", "mailext.json");
     const res = await updateSchemas();
     const {callCount: writeCallCount} = stubWrite;
     stubWrite.restore();
     stubAll.restore();
-    assert.strictEqual(writeCallCount, writeCount + 3, "write");
-    assert.deepEqual(res, [betaPath, centralPath, releasePath], "result");
+    assert.strictEqual(writeCallCount, writeCount + 4, "write");
+    assert.deepEqual(res, [betaPath, centralPath, releasePath, mailPath],
+                     "result");
   });
 
   it("should get result", async () => {
     const stubAll = sinon.stub(Promise, "all").callsFake(async arr => {
       let val;
       switch (arr.length) {
+        case 3:
+          val = [
+            [
+              {
+                file: "foo",
+                schema: {
+                  foo: "baz",
+                },
+              },
+            ],
+            [
+              {
+                file: "baz",
+                schema: {
+                  baz: "qux",
+                },
+              },
+            ],
+            [
+              {
+                file: "foobar",
+                schema: {
+                  foobar: "quux",
+                },
+              },
+            ],
+          ];
+          break;
         case 2:
           val = [
             [
