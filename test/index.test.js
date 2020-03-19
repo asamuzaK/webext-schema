211d29e1bc17e55785a4241073d352575098f05a
asamuzaK (Kazz) <kazz@asamuzak.jp>
2020-03-19 09:04:01 +0900

    Revert "Delete index.test.js"
    This reverts commit 0b9f364f219fb0289e3fca091b1aaccf238b07b4.

---
+++ test/index.test.js
@@ -0,0 +1,14 @@
+"use strict";
+/* api */
+const {
+  Schema,
+} = require("../index");
+const {assert} = require("chai");
+const {describe, it} = require("mocha");
+
+describe("Schema", () => {
+  it("should be instance of Schema", () => {
+    const schema = new Schema();
+    assert.instanceOf(schema, Schema);
+  });
+});
