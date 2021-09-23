e52164975dfa4ddc95cd9a69aa83ab8b12cc4af6
asamuzaK (Kazz) <kazz@asamuzak.jp>
2021-09-24 08:45:27 +0900

    Update index.js

--- index.js
+++ index.js
@@ -3,17 +3,13 @@
  */
 
 /* api */
-import { Schema } from './modules/schema.js';
 import { logErr, throwErr } from './modules/common.js';
 import { parseCommand } from './modules/update.js';
 import process from 'process';
+export { Schema } from './modules/schema.js';
 
 /* process */
 process.on('uncaughtException', throwErr);
 process.on('unhandledRejection', logErr);
 
 parseCommand(process.argv);
-
-export {
-  Schema
-};
