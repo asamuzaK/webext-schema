1811b100bfec3eb6504a80d49f4908bbf4d3de82
asamuzaK (Kazz) <asamuzaK@users.noreply.github.com>
2024-09-28 22:30:20 +0900

    Update eslint to v9 (#11)

--- test/browser.test.js
+++ test/browser.test.js
@@ -9,7 +9,7 @@ import { afterEach, beforeEach, describe, it } from 'mocha';
 import { browser } from './mocha/setup.js';
 
 /* test */
-// eslint-disable-next-line import/order
+// eslint-disable-next-line import-x/order
 import * as mjs from '../modules/browser.js';
 
 describe('browser', () => {
